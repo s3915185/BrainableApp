bcf9d92bd066cf7a3cdb5b9d7692956be9ac7848
s3915185 <tran.vuhoangsf19@gmail.com>
2023-08-11 09:26:04 +0700

    Initial Commit

---
+++ BrainableApp/BrainableAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainableAppApp.swift
+//  BrainableApp
+//
+//  Created by Vu Tran Hoang on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BrainableAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainableApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BrainableApp
+//
+//  Created by Vu Tran Hoang on 11/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
